1031275e0e7add51ded968695ebc8539ed13ef45
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-03-23 15:38:57 -0400

    fix solidity lint

--- contracts/BasisTrade.sol
+++ contracts/BasisTrade.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.2;
 pragma abicoder v2;
 
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 contract BasisTrade {
     ISwapRouter public immutable swapRouter;
@@ -14,50 +14,53 @@ contract BasisTrade {
 
     uint24 public constant poolFee = 3000;
 
-    constructor(ISwapRouter _swapRouter, address _DAI, address _WETH9) {
+    constructor(
+        ISwapRouter _swapRouter,
+        address _DAI,
+        address _WETH9
+    ) {
         swapRouter = _swapRouter;
         DAI = _DAI;
         WETH9 = _WETH9;
     }
 
     function swapExactInputSingle(uint256 amountIn) internal returns (uint256 amountOut) {
-
         TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
 
         TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
 
-        ISwapRouter.ExactInputSingleParams memory params =
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: DAI,
-                tokenOut: WETH9,
-                fee: poolFee,
-                recipient: msg.sender,
-                deadline: block.timestamp,
-                amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: DAI,
+            tokenOut: WETH9,
+            fee: poolFee,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountIn: amountIn,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
 
         amountOut = swapRouter.exactInputSingle(params);
     }
 
-    function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum) internal returns (uint256 amountIn) {
-
+    function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum)
+        internal
+        returns (uint256 amountIn)
+    {
         TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountInMaximum);
 
         TransferHelper.safeApprove(DAI, address(swapRouter), amountInMaximum);
 
-        ISwapRouter.ExactOutputSingleParams memory params =
-            ISwapRouter.ExactOutputSingleParams({
-                tokenIn: DAI,
-                tokenOut: WETH9,
-                fee: poolFee,
-                recipient: msg.sender,
-                deadline: block.timestamp,
-                amountOut: amountOut,
-                amountInMaximum: amountInMaximum,
-                sqrtPriceLimitX96: 0
-            });
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter.ExactOutputSingleParams({
+            tokenIn: DAI,
+            tokenOut: WETH9,
+            fee: poolFee,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountOut: amountOut,
+            amountInMaximum: amountInMaximum,
+            sqrtPriceLimitX96: 0
+        });
 
         amountIn = swapRouter.exactOutputSingle(params);
 
