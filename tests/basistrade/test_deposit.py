8816300da4f3cc638c41405796fb229026badc7d
deepsp94 <anantsp94@gmail.com>
2022-04-17 01:21:57 +0530

    Deposit tests

--- tests/basistrade/test_deposit.py
+++ tests/basistrade/test_deposit.py
@@ -13,20 +13,18 @@ def test_deposit(rando1, rando2, eth_basis_trade, weth, amount):
     # approve weth for spending
     weth.approve(eth_basis_trade.address, amount, {'from': rando1})
     # deposit weth to basis trade contract
-    eth_basis_trade.depositEth(amount, {'from': rando1})
+    eth_basis_trade.depositWeth(amount, {'from': rando1})
     # tests
     assert weth.balanceOf(eth_basis_trade) == rando1_initial_balance
-    assert eth_basis_trade.depositorInfo(rando1)[0] == amount
-    assert eth_basis_trade.depositorInfo(rando1)[1] == True
-    assert eth_basis_trade.depositorInfo(rando1)[2] == False
+    assert eth_basis_trade.depositorAddressPre(0) == rando1.address
+    assert eth_basis_trade.depositorInfoPre(rando1) == amount
 
     # repeat the process for rando2
     weth_token._mint_for_testing(rando2, amount)
     rando2_initial_balance = weth.balanceOf(rando2)
     weth.approve(eth_basis_trade.address, amount, {'from': rando2})
-    eth_basis_trade.depositEth(amount, {'from': rando2})
+    eth_basis_trade.depositWeth(amount, {'from': rando2})
     tot_bal = rando1_initial_balance + rando2_initial_balance
     assert weth.balanceOf(eth_basis_trade) == tot_bal
-    assert eth_basis_trade.depositorInfo(rando2)[0] == amount
-    assert eth_basis_trade.depositorInfo(rando2)[1] == True
-    assert eth_basis_trade.depositorInfo(rando2)[2] == False
+    assert eth_basis_trade.depositorAddressPre(1) == rando2.address
+    assert eth_basis_trade.depositorInfoPre(rando2) == amount
